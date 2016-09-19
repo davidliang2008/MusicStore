10e3fd1119824cd89c9e70767394b435808ca8a3
davidliang2008 <david.liang@outlook.com>
2016-09-18 18:40:15 -0700

    Add artist mapping configuration.

--- DL.MusicStore.Web.Mvc/Controllers/HomeController.cs
+++ DL.MusicStore.Web.Mvc/Controllers/HomeController.cs
@@ -12,35 +12,6 @@ namespace DL.MusicStore.Web.Mvc.Controllers
         // This is just for demo purpose
         public ActionResult Index()
         {
-            // To create an artist
-            Artist artist = new Artist("David Liang");
-            //Artist artist = new Artist();
-            //artist.ArtistId = 1;
-            //artist.Name = "David Liang";
-
-            // To create the style
-            AlbumStyle rockStyle = new AlbumStyle("Rock");
-
-            //rockStyle.AlbumStyleId = 1;
-            //rockStyle.Name = "Rock";
-
-            // To create an album
-            Album testAlbum = new Album("Test Album", 12.5, DateTime.Now);
-            //Album testAlbum = new Album();
-            //testAlbum.AlbumId = 1;
-            //testAlbum.Title = "Test Album";
-            //testAlbum.Price = 12.5;
-            //testAlbum.DatePublished = DateTime.Now;
-
-            //testAlbum.Artist = artist;
-            //testAlbum.Style = rockStyle;
-
-            //......
-
-            // Later on in the process, if somebody changes Album's id = 2,
-            // it's not dorable after we use private keyword.
-            //testAlbum.AlbumId = 2;
-
             return View();
         }
     }
