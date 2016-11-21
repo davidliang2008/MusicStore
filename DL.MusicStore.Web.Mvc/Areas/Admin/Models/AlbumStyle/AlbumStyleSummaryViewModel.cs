39eb33b0c3b0134af2ca63bd9f7675bda83d4648
davidliang2008 <david.liang@outlook.com>
2016-11-20 23:57:56 -0800

    Display the album style list from database.

---
+++ DL.MusicStore.Web.Mvc/Areas/Admin/Models/AlbumStyle/AlbumStyleSummaryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DL.MusicStore.Web.Mvc.Areas.Admin.Models.AlbumStyle
+{
+    public class AlbumStyleSummaryViewModel
+    {
+        public int AlbumStyleId { get; set; }
+        public string AlbumStyleName { get; set; }
+    }
+}
