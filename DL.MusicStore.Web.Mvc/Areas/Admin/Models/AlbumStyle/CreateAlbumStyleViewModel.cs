a7c01ca161404d173e5194c4026a0ce499922d4a
davidliang2008 <david.liang@outlook.com>
2016-11-28 22:46:30 -0800

    Craft out create album style page.

---
+++ DL.MusicStore.Web.Mvc/Areas/Admin/Models/AlbumStyle/CreateAlbumStyleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DL.MusicStore.Web.Mvc.Areas.Admin.Models.AlbumStyle
+{
+    public class CreateAlbumStyleViewModel
+    {
+        [Display(Name = "Name of style")]
+        public string StyleName { get; set; }
+    }
+}
