2f7c8ac710687aa5e87502a5328e444d48cb678b
davidliang2008 <david.liang@outlook.com>
2017-03-12 14:30:29 -0700

    Install Font Awesome and define breakpoints for sidebar to collapse.

---
+++ DL.MusicStore.Web.Mvc/Scripts/assets/admin/init.js
@@ -0,0 +1,8 @@
+﻿// When the DOM loads
+$(function () {
+    // Setup event bindings
+    $('#sidebar-toggler').click(function () {
+        $('body').toggleClass('sidebar-open');
+        return false;
+    });
+});
